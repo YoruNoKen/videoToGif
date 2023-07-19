04bd476d3d3c355ee6134f56f9815215ef25cd57
Yoru <m.f.fetvaci@hotmail.com>
2023-07-20 00:17:58 +0300

    added ffmpeg check

--- setup.js
+++ setup.js
@@ -12,9 +12,21 @@ if (!fs.existsSync(nodeModulesDir)) {
     }
     console.log("npm install completed successfully.");
 
-    exec("npm start");
+    startProgram();
   });
 } else {
   console.log("Node modules already installed, continuing...");
-  exec("npm start");
+  startProgram();
+}
+
+function startProgram() {
+  const command = "ffmpeg -version";
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error("FFmpeg not found. Please make sure it is installed and added to your system PATH.");
+    } else {
+      exec("npm start");
+    }
+  });
 }
